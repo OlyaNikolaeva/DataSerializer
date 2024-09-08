42ed2cc51a5995151f818637277e05ff6333a0b7
olya_nick <olya17nikolbas@gmail.com>
2024-09-08 18:00:19 +0300

    Добавлены методы сериализации. Добавлены примеры

---
+++ DataCsvSerializer/CsvSerializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCsvSerializer
+{
+    public class CsvSerializer
+    {
+        public static string Serialize<T>(T obj)
+        {
+            var sb = new StringBuilder();
+            var type = typeof(T);
+            // Для использования public и private полей
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (var field in fields)
+            {
+                var value = field.GetValue(obj);
+                sb.Append(value?.ToString() + ",");
+            }
+
+            return sb.ToString().TrimEnd(',');
+        }
+
+        public static T Deserialize<T>(string csv) where T : new()
+        {
+            var values = csv.Split(',');
+            var type = typeof(T);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            var obj = new T();
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                var fieldType = fields[i].FieldType;
+                var value = Convert.ChangeType(values[i], fieldType);
+                fields[i].SetValue(obj, value);
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ DataCsvSerializer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace DataCsvSerializer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = F.Get();
+            var iterations = 100000;
+
+            // Замер времени на сериализацию
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < iterations; i++)
+            {
+                var csv = CsvSerializer.Serialize(instance);
+            }
+            sw.Stop();
+            Console.WriteLine($"CSV Serialization Time: {sw.ElapsedMilliseconds} ms");
+
+            // Замер времени на десериализацию
+            var csvSerialized = CsvSerializer.Serialize(instance);
+            sw.Restart();
+            for (int i = 0; i < iterations; i++)
+            {
+                var deserialized = CsvSerializer.Deserialize<F>(csvSerialized);
+            }
+            sw.Stop();
+            Console.WriteLine($"CSV Deserialization Time: {sw.ElapsedMilliseconds} ms");
+
+            // Сравнение с Newtonsoft.Json
+            sw.Restart();
+            for (int i = 0; i < iterations; i++)
+            {
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(instance);
+            }
+            sw.Stop();
+            Console.WriteLine($"JSON Serialization Time (Newtonsoft): {sw.ElapsedMilliseconds} ms");
+
+            var jsonSerialized = Newtonsoft.Json.JsonConvert.SerializeObject(instance);
+            sw.Restart();
+            for (int i = 0; i < iterations; i++)
+            {
+                var deserialized = Newtonsoft.Json.JsonConvert.DeserializeObject<F>(jsonSerialized);
+            }
+            sw.Stop();
+            Console.WriteLine($"JSON Deserialization Time (Newtonsoft): {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
